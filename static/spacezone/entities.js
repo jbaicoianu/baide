da43479e73b5360809d005b5e670527e776c5abd
James Baicoianu <james_github@baicoianu.com>
2025-03-03 10:14:19 -0800

    Added placeholder sphere to the `spacezone-waypoint` element.

--- static/spacezone/entities.js
+++ static/spacezone/entities.js
@@ -30,8 +30,14 @@ room.registerElement('spacezone-waypoint', {
   create() {
     // Initialization code for spacezone-waypoint
     this.position = new THREE.Vector3();
+    this.placeholderSphere = this.createObject('sphere', {
+      pos: this.position,
+      scale: new THREE.Vector3(0.5, 0.5, 0.5),
+      col: 'green'
+    });
   },
   update(dt) {
     // Update logic for spacezone-waypoint
+    // You can add interactions or animations for the placeholder sphere here
   }
 });
