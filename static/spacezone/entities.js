cd60100881863f5b31be796c7f842b6ba89bda48
James Baicoianu <james_github@baicoianu.com>
2025-03-04 10:32:27 -0800

    Added `rotate_deg_per_sec` of `5` to the sphere in the `spacezone-planet` element to enable rotation.

--- static/spacezone/entities.js
+++ static/spacezone/entities.js
@@ -266,6 +266,7 @@ room.registerElement('spacezone-planet', {
       id: 'sphere',
       image_id: this.surfacetexture,
       scale: V(this.radius * 2),
+      rotate_deg_per_sec: V(0, 5, 0)
     });
   },
   update(dt) {
