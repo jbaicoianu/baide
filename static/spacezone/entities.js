08bfa65af1b82974a53f427cecef49c48bfefdc9
James Baicoianu <james_github@baicoianu.com>
2025-03-25 16:09:40 -0700

    Tweaked trail particle velocity

--- static/spacezone/entities.js
+++ static/spacezone/entities.js
@@ -581,7 +581,7 @@ room.registerElement('spacezone-enginetrail', {
       duration: 1.9, // Updated duration to 1.9
       opacity: 0.2,
       vel: V(0, 0, 2.5), // Set vel to V(0,0,2.5)
-      rand_vel: V(0, 0, 2.5), // Set rand_vel to V(0,0,2.5)
+      rand_vel: V(0, 0, 5), // Set rand_vel to V(0,0,2.5)
       col: 'cyan', // Set particle color to cyan
       image_id: 'spark' // Set image_id to 'spark'
     });
