20c25016e966cea6006a7a7c84cdef1f5ec00252
James Baicoianu <james_github@baicoianu.com>
2025-03-27 12:14:05 -0700

    Tweaked base asteroid vertex offsets

--- static/spacezone/entities.js
+++ static/spacezone/entities.js
@@ -488,9 +488,9 @@ room.registerElement('spacezone-asteroidfield', {
           const currX = pos.getX(j);
           const currY = pos.getY(j);
           const currZ = pos.getZ(j);
-          const x = currX + (0 - Math.random() * .75);
-          const y = currY + (0 - Math.random() * .75);
-          const z = currZ + (0 - Math.random() * .75);
+          const x = currX + (0 - Math.random() * .5);
+          const y = currY + (0 - Math.random() * .5);
+          const z = currZ + (0 - Math.random() * .5);
 
           pos.setX(j, x);
           pos.setY(j, y);
