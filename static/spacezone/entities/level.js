f39c651c0d9a21dd50bc9f7191bbc49d8010a992
James Baicoianu <james_github@baicoianu.com>
2025-04-01 22:45:17 -0700

    Tweaked portal parameters

--- static/spacezone/entities/level.js
+++ static/spacezone/entities/level.js
@@ -54,19 +54,20 @@ room.registerElement('spacezone-level', {
     });
 
     // Add two large portals behind the cargo ship
-    this.portalLeft = this.createObject('link', {
+    this.portalRight = this.createObject('link', {
       pos: cargoShipPosition.clone().add(V(-200, 0, 200)),
       scale: V(100),
       round: true,
       shader_id: 'defaultportal',
-      url: 'https://portal.pieter.com/',
     });
 
-    this.portalRight = this.createObject('link', {
+    this.portalLeft = this.createObject('link', {
       pos: cargoShipPosition.clone().add(V(200, 0, 200)),
       scale: V(100),
       round: true,
       shader_id: 'defaultportal'
+      url: 'https://portal.pieter.com/',
+      external: true,
     });
 
     // // Generate 10 enemy drones positioned randomly along the path
