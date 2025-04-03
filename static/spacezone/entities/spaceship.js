e19593e8fe750de9a6a135c2e3993203ad93209e
James Baicoianu <james_github@baicoianu.com>
2025-04-03 11:31:50 -0700

    Shield tweaks

--- static/spacezone/entities/spaceship.js
+++ static/spacezone/entities/spaceship.js
@@ -96,7 +96,7 @@ room.registerElement('spacezone-spaceship', {
       scale: V(16, 3.5, 18),
       col: 'red',
       lighting: false,
-      opacity: 0.2,
+      opacity: 0.1,
       renderorder: 10,
       //shader_id: 'shield-shader'
     });
