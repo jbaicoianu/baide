02a231e1c26f3cc47afb9adaaf58711d400fba9f
James Baicoianu <james_github@baicoianu.com>
2025-04-11 22:29:29 -0700

    Move explosion into its own file

--- static/spacezone/entities/spaceship.js
+++ static/spacezone/entities/spaceship.js
@@ -1718,26 +1718,3 @@ room.registerElement('spacezone-targeting-reticle', {
   }
 });
 
-room.registerElement('explosion', {
-  count: 50,
-  create() {
-    // Initialize the particle system for the explosion
-    this.particles = this.createObject('particle', {
-      count: this.count,
-      rate: 20000,
-      loop: false,
-      col: this.col,
-      rand_col: V(0.25),
-      vel: V(-50),
-      rand_vel: V(100),
-      duration: 20,
-      scale: V(5),
-      image_id: 'spark'
-    });
-  },
-  reset() {
-    // Reset and start the particle system
-    this.particles.resetParticles();
-    this.particles.start();
-  }
-});
