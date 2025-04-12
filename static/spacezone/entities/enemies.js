49d47089cbc1bd82cd2dafb53a7c12f7c70dffd9
James Baicoianu <james_github@baicoianu.com>
2025-04-12 10:15:43 -0700

    Tweak mine explosion damage

--- static/spacezone/entities/enemies.js
+++ static/spacezone/entities/enemies.js
@@ -394,7 +394,7 @@ room.registerElement('spacezone-enemy-mine', {
     this.exploded = true;
     
     const distance = this.distanceTo(this.player);
-    const damageAmount = Math.max(100 - Math.pow(distance / 5, 2), 0); // Ensure damage is not negative
+    const damageAmount = Math.max(100 - Math.pow(distance / 4, 2), 0); // Ensure damage is not negative
       
     // Dispatch damage event to the player
     if (damageAmount > 0) {
