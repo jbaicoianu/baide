1e04697cd73faee1c6c097fd38c346a3abf23397
James Baicoianu <james_github@baicoianu.com>
2025-05-05 22:26:12 -0700

    Sky shader tweaks

--- static/janusweb/weather-metar.js
+++ static/janusweb/weather-metar.js
@@ -477,7 +477,7 @@ room.registerElement('weather-metar', {
                 shader_id: 'clouds',
                 image_id: 'skynoise',
                 cull_face: 'front',
-                pos: `0 ${-(scale/2) + altitude} 0`,
+                pos: `0 ${(-scale + altitude) / 2} 0`,
                 scale: `${scale} ${scale} ${scale}`,
                 col: color,
                 transparent: true,
