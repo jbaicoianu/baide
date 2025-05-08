5c737478c0641e3f3dcc94a269c02b7ee5bde625
James Baicoianu <james_github@baicoianu.com>
2025-05-08 15:25:38 -0700

    Updated skydome

--- static/janusweb/weather-metar.js
+++ static/janusweb/weather-metar.js
@@ -677,7 +677,7 @@ console.log('my wind!', winddir, windspeed, weather.windSpeedKts, adjustedWindsp
 room.registerElement('weather-skybox', {
     resolution: 1024,
     stationid: 'KOAK',
-    refreshtime: 1/10,
+    refreshtime: 10,
     
     create() {
         var options = {
