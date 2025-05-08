722e8fdb87100a8d3ae11d4d4a5a803c514b5af9
James Baicoianu <james_github@baicoianu.com>
2025-05-08 14:16:33 -0700

    Updated skydome

--- static/janusweb/weather-metar.js
+++ static/janusweb/weather-metar.js
@@ -714,7 +714,7 @@ room.registerElement('weather-skybox', {
 	    let renderer = this.engine.systems.render.renderer;
 	    this.cubeCamera.update(renderer, this.skyscene);
         //console.log('feh', this.cubeCamera, this.skyscene);
-        this.cube.position.y = Math.sin(Date.now());
+        this.cube.position.y = Math.sin(Date.now() / 1000);
     },
   	update(dt) {
     	if (this.cubeCamera && dt <= 1/30) {
